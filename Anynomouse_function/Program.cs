3b3a15ea54bea8bfd70b8016a8aacbe86a2b26f8
itemad hyder <itemad.hyder1997@gmail.com>
2023-02-21 13:18:00 +0530

    first Commit

---
+++ Anynomouse_function/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Anynomouse_function
+{
+    public delegate void Calculation( int b);
+    class Program
+    {
+        public static void Addition(Calculation del ,int b)
+        {
+            Console.WriteLine("Addition of Two number {0}",b);
+        }
+
+        static void Main(string[] args)
+        {
+            Calculation obj = delegate (int a)
+            {
+                a += 10;
+                Console.WriteLine(a);
+
+            };
+            obj.Invoke(10);
+        }
+    }
+}
